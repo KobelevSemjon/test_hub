fb5f00aa5abf08075c5b215505a6c02fa763c34c
Кобелев Семен <33935798+KobelevSemjon@users.noreply.github.com>
2017-11-23 20:08:55 +0300

    bubu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <stdlib.h>
 
+using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    cout<<"Hello World!"<<endl;
 
     return a.exec();
 }
